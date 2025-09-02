ad6f2a1e3557993598afffa2326bc810579bcfd4
James Macfie <1654201+jamesmacfie@users.noreply.github.com>
2025-09-02 19:26:59 +1200

    .env ts changes

--- extension-env.d.ts
+++ extension-env.d.ts
@@ -7,24 +7,3 @@
 
 // Polyfill types for browser.* APIs
 /// <reference types="extension/types/polyfill" />
-
-// Browser extension API globals
-declare const chrome: any
-
-// Browser API types for cross-compatibility
-declare namespace browser {
-  namespace runtime {
-    type MessageSender = chrome.runtime.MessageSender
-  }
-}
-
-// Extended browser API that includes Firefox-specific features
-declare const browser: typeof chrome & {
-  tabs: typeof chrome.tabs & {
-    toggleReaderMode?: (tabId: number) => Promise<void>
-    saveAsPDF?: (options: any) => Promise<void>
-  }
-  contextualIdentities?: {
-    query: (queryInfo: any) => Promise<any[]>
-  }
-}
